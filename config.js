259749e234e44b8b70ec8a62d04d6932643686a3
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-09-30 21:15:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "1VUDVazC#B0N9V8Jl9XP9ZjsK3jV88Z8m-wNcGijdHwhlTILqJfA",
+SESSION_ID: process.env.SESSION_ID || "agni~fQkzQLzS#9dqIYBb5R0McZVUdkWNg8GddWWz-qpYX3FAXH7MeKuk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
